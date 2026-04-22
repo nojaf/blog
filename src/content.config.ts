8ecfda4be2986f31cd1872b3404d27611d290382
nojaf <florian.verdonck@outlook.com>
2026-04-22 11:47:47 +0200

    Bump Astro to v6

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,9 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/blog" }),
   // Type-check frontmatter using a schema
   schema: ({ image }) =>
     z.object({
